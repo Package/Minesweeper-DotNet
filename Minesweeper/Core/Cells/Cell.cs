f253c7b315b3fd9331038e02faa1f139ff20a81d
Package <ryan.dean1000@outlook.com>
2020-11-01 21:05:07 +0000

    Got game working with graphics

--- Minesweeper/Core/Cells/Cell.cs
+++ Minesweeper/Core/Cells/Cell.cs
@@ -6,47 +6,53 @@ using System.Windows.Forms;
 
 namespace Minesweeper.Core
 {
-    public class Cell : Button
+    public class Cell
     {
-        private readonly Font _defaultFont = new Font("Verdana", 14f, FontStyle.Bold);
-        private readonly Font _smallFont = new Font("Arial", 8f, FontStyle.Bold);
-
         public int XLoc { get; set; }
         public int YLoc { get; set; }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public Point CenterPos { get; set; }
+        public Point TopLeftPos { get; set; }
+        public Point BottomLeftPos { get; set; }
         public int CellSize { get; set; }
         public CellState CellState { get; set; }
         public CellType CellType { get; set; }
         public int NumMines { get; set; }
         public Board Board { get; set; }
         public double MinePercentage { get; set; }
+        public Rectangle Bounds { get; private set; }
+        private List<Cell> Surrounding { get; set; }
+        public bool Flagged => CellType == CellType.Flagged || CellType == CellType.FlaggedMine;
 
-        /// <summary>
-        /// Setup the look and feel for the buttons.
-        /// Should be called just once after initializing the cells.
-        /// </summary>
-        public void SetupDesign()
-        {
-            this.Location = new Point((XLoc * CellSize) + CellSize, (YLoc * CellSize) + CellSize);
-            this.Size = new Size(CellSize, CellSize);
-            this.UseVisualStyleBackColor = false;
-            this.TextAlign = ContentAlignment.MiddleCenter;
-            this.Font = _defaultFont;
-            
-            // Borders
-            this.BackColor = Color.Silver;
-            this.FlatStyle = FlatStyle.Flat;
-            this.FlatAppearance.BorderColor = Color.Gray;
-            this.FlatAppearance.BorderSize = 1;
-        }
+        public bool Closed => CellState == CellState.Closed;
+
+        public bool Opened => CellState == CellState.Opened;
 
         /// <summary>
         /// Return whether the type of this cell is a Mine.
         /// </summary>
         /// <returns></returns>
-        public bool IsMine()
+        public bool IsMine => CellType == CellType.Mine || CellType == CellType.FlaggedMine;
+
+        /// <summary>
+        /// Constructs a new <see cref="Cell"/>
+        /// </summary>
+        public Cell(int x, int y, Board board)
         {
-            return this.CellType == CellType.Mine ||
-                this.CellType == CellType.FlaggedMine;
+            XLoc = x;
+            YLoc = y;
+            CellSize = Board.CellSize;
+            CellState = CellState.Closed;
+            CellType = CellType.Regular;
+            MinePercentage = -1;
+            Bounds = new Rectangle(XLoc * CellSize, YLoc * CellSize, CellSize, CellSize);
+            Board = board;
+            XPos = XLoc * CellSize;
+            YPos = YLoc * CellSize;
+            CenterPos = new Point(XPos + (CellSize / 2 - 10), YPos + (CellSize / 2 - 10));
+            TopLeftPos = new Point(XPos, YPos);
+            BottomLeftPos = new Point(XPos, YPos + (CellSize - 10));
         }
 
         /// <summary>
@@ -54,7 +60,7 @@ namespace Minesweeper.Core
         /// </summary>
         public void OnFlag()
         {
-            switch (this.CellType)
+            switch (CellType)
             {
                 case CellType.Regular:
                     this.CellType = CellType.Flagged;
@@ -72,7 +78,7 @@ namespace Minesweeper.Core
                     throw new Exception($"Unknown cell type {this.CellType}");
             }
 
-            this.UpdateDisplay();
+            Board.Minesweeper.Invalidate();
         }
 
         /// <summary>
@@ -83,45 +89,31 @@ namespace Minesweeper.Core
             // Recursive cell opening stops when it gets to a non-regular cell or a cell that's already open.
             if (recursiveCall)
             {
-                if (this.CellType != CellType.Regular || this.CellState != CellState.Closed)
+                if (CellType != CellType.Regular || CellState != CellState.Closed)
                     return;
             }
 
             // Cell was a mine
-            if (this.CellType == CellType.Mine)
+            if (CellType == CellType.Mine)
             {
-                this.CellState = CellState.Opened;
-                this.UpdateDisplay();
-
-                // For each cell, reveal whether it was a bomb or not
-                for (var x = 0; x < this.Board.Width; x++)
-                {
-                    for (var y = 0; y < this.Board.Height; y++)
-                    {
-                        this.Board.Cells[x, y].CellState = CellState.Opened;
-                        this.Board.Cells[x, y].UpdateDisplay();
-                    }
-                }
-
-                MessageBox.Show("You have opened a mine! Good game.");
-
-                Board.RestartGame();
+                CellState = CellState.Opened;
+                Board.RevealMines();
                 return;
             }
 
             // Regular cell
-            if (this.CellType == CellType.Regular)
+            if (CellType == CellType.Regular)
             {
-                this.CellState = CellState.Opened;
-                this.UpdateDisplay();
+                CellState = CellState.Opened;
             }
 
             // Recursively open surrounding cells.
-            if (this.NumMines == 0)
+            if (NumMines == 0)
             {
-                var neighbors = this.GetNeighborCells();
-                foreach (var n in neighbors)
+                foreach (var n in GetNeighborCells())
+                {
                     n.OnClick(true);
+                }
             }
         }
 
@@ -131,111 +123,35 @@ namespace Minesweeper.Core
         /// <returns></returns>
         public List<Cell> GetNeighborCells()
         {
-            var neighbors = new List<Cell>();
-
-            for (var x = -1; x < 2; x++)
+            if (Surrounding == null)
             {
-                for (var y = -1; y < 2; y++)
+                Surrounding = new List<Cell>();
+
+                for (var x = -1; x < 2; x++)
                 {
-                    // Can't be your own neighbor!
-                    if (x == 0 && y == 0)
-                        continue;
+                    for (var y = -1; y < 2; y++)
+                    {
+                        // Can't be your own neighbor!
+                        if (x == 0 && y == 0)
+                            continue;
 
-                    // Cell would be out of bounds
-                    if (XLoc + x < 0 || XLoc + x >= Board.Width || YLoc + y < 0 || YLoc + y >= Board.Height)
-                        continue;
+                        // Cell would be out of bounds
+                        if (XLoc + x < 0 || XLoc + x >= Board.Width || YLoc + y < 0 || YLoc + y >= Board.Height)
+                            continue;
 
-                    neighbors.Add(Board.Cells[XLoc + x, YLoc + y]);
+                        Surrounding.Add(Board.Cells[XLoc + x, YLoc + y]);
+                    }
                 }
             }
 
-            return neighbors;
-        }
-
-        /// <summary>
-        /// Updates the display of the cell.
-        /// </summary>
-        public void UpdateDisplay()
-        {
-            this.Font = _defaultFont;
-            this.TextAlign = ContentAlignment.MiddleCenter;
-
-            // Cell is flagged
-            if (this.CellType == CellType.Flagged ||
-                this.CellType == CellType.FlaggedMine)
-            {
-                this.ForeColor = Color.Black;
-                this.Text = "?";
-                return;
-            }
-
-            // Cell is closed
-            if (this.CellState == CellState.Closed)
-            {
-                this.BackColor = Color.DarkGray;
-                this.Text = string.Empty;
-
-                if (this.MinePercentage != -1)
-                {
-                    this.ForeColor = Color.Red;
-                    this.Font = _smallFont;
-                    this.Text = $"{this.MinePercentage}%";
-                    this.TextAlign = ContentAlignment.TopLeft;
-                }
-
-            }
-
-            // Open mine
-            if (this.CellType == CellType.Mine && (this.CellState == CellState.Opened || Board.ShowMines))
-            {
-                this.BackColor = Color.DarkGray;
-                this.ForeColor = Color.DarkRed;
-                this.Text = "M";
-            }
-
-            // Open regular cell (show number of mines around it)
-            if (this.CellType == CellType.Regular && this.CellState == CellState.Opened)
-            {
-                this.BackColor = Color.LightGray;
-                this.ForeColor = this.GetCellColour();
-                this.Text = this.NumMines > 0 ? $"{this.NumMines}" : string.Empty;
-            }
-        }
-
-        /// <summary>
-        /// Return the colour code associated with the number of surrounding mines
-        /// </summary>
-        /// <returns></returns>
-        private Color GetCellColour()
-        {
-            switch (this.NumMines)
-            {
-                case 1:
-                    return ColorTranslator.FromHtml("0x0000FE"); // 1
-                case 2:
-                    return ColorTranslator.FromHtml("0x186900"); // 2
-                case 3:
-                    return ColorTranslator.FromHtml("0xAE0107"); // 3
-                case 4:
-                    return ColorTranslator.FromHtml("0x000177"); // 4
-                case 5:
-                    return ColorTranslator.FromHtml("0x8D0107"); // 5
-                case 6:
-                    return ColorTranslator.FromHtml("0x007A7C"); // 6
-                case 7:
-                    return ColorTranslator.FromHtml("0x902E90"); // 7
-                case 8:
-                    return ColorTranslator.FromHtml("0x000000"); // 8
-                default:
-                    return ColorTranslator.FromHtml("0xffffff");
-            }
+            return Surrounding;
         }
 
         /// <summary>
         /// Work out the percentage % of this cell being a mine.
         /// </summary>
         /// <returns></returns>
-        public double CalculateMinePercentage()
+        public void CalculateMinePercentage()
         {
             double pct = 0d;
             int checkedCells = 0;
@@ -260,11 +176,17 @@ namespace Minesweeper.Core
 
                 // 0% chance of being a mine
                 if (flaggedCells == surroundingMines)
-                    return 0;
+                {
+                    MinePercentage = 0;
+                    return;
+                }
 
                 // 100% of being a mine
                 if (surroundingMines == (availCells + flaggedCells))
-                    return 100;
+                {
+                    MinePercentage = 100;
+                    return;
+                }
 
                 checkedCells += 1;
                 pct += (surroundingMines * 1.0 / availCells) * 100;
@@ -273,10 +195,11 @@ namespace Minesweeper.Core
             // Unable to determine - did not consider any cells.
             if (checkedCells == 0)
             {
-                return -1;
+                MinePercentage = -1;
+                return;
             }
 
-            return Math.Round(pct / (checkedCells > 0 ? checkedCells : 1));
+            MinePercentage = Math.Round(pct / (checkedCells > 0 ? checkedCells : 1));
         }
     }
 }
--- Minesweeper/Forms/Minesweeper.cs
+++ Minesweeper/Forms/Minesweeper.cs
@@ -1,29 +1,25 @@
 ﻿using Minesweeper.Core;
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Minesweeper
 {
     public partial class Minesweeper : Form
     {
-        public Board _board { get; set; }
+        public Board _board { get; private set; }
 
-        public GameMode _mode { get; set; }
+        public GameMode _mode { get; private set; }
 
-        public int BOARD_WIDTH { get; set; }
-        public int BOARD_HEIGHT { get; set; }
-        public int NUM_MINES { get; set; }
+        public int BOARD_WIDTH { get; private set; }
+        public int BOARD_HEIGHT { get; private set; }
+        public int NUM_MINES { get; private set; }
 
-        public Random _random { get; set; }
+        public Random _random { get; private set; }
 
         public Minesweeper()
         {
             InitializeComponent();
+            DoubleBuffered = true;
 
             _random = new Random();
             _mode = GameMode.Intermediate;
@@ -62,7 +58,6 @@ namespace Minesweeper
             }
         }
 
-
         /// <summary>
         /// Reveals the locations of the mines.
         /// </summary>
@@ -71,15 +66,7 @@ namespace Minesweeper
         private void btnShowMines_Click(object sender, EventArgs e)
         {
             _board.ShowMines = !_board.ShowMines;
-
-            for (int x = 0; x < BOARD_WIDTH; x++)
-            {
-                for (int y = 0; y < BOARD_HEIGHT; y++)
-                {
-                    var cc = this._board.Cells[x, y];
-                    cc.UpdateDisplay();
-                }
-            }
+            Invalidate();
         }
 
         /// <summary>
@@ -89,7 +76,74 @@ namespace Minesweeper
         /// <param name="e"></param>
         private void btnShowPercent_Click(object sender, EventArgs e)
         {
-            this._board.SetMinePercentages();
+            _board.ShowPercentage = !_board.ShowPercentage;
+            _board.SetMinePercentages();
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Handles click events on the form for opening/flagging cells.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Minesweeper_Click(object sender, EventArgs e)
+        {
+            var mouseArgs = (MouseEventArgs)e;
+
+            var clickedX = mouseArgs.X - Board.CellSize;
+            var clickedY = mouseArgs.Y - Board.CellSize;
+
+            var cellX = clickedX / Board.CellSize;
+            var cellY = clickedY / Board.CellSize;
+
+            // Check for out of bounds:
+            if (clickedX < 0 || clickedY < 0 || cellX < 0 || cellY < 0 || cellX >= _board.Width || cellY >= _board.Height)
+            {
+                return;
+            }
+
+            switch (mouseArgs.Button)
+            {
+                case MouseButtons.Left:
+                    // Left click opens the cell:
+                    _board.Cells[cellX, cellY].OnClick();
+                    AfterClick();
+                    break;
+                case MouseButtons.Right:
+                    // Right click places a flag:
+                    _board.Cells[cellX, cellY].OnFlag();
+                    AfterClick();
+                    break;
+                default: 
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Updates the UI after the user has clicked
+        /// </summary>
+        private void AfterClick()
+        {
+            if (_board.ShowPercentage)
+            {
+                _board.SetMinePercentages();
+            }
+
+            _board.CheckForWin();
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Paints the game board.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Minesweeper_Paint(object sender, PaintEventArgs e)
+        {
+            if (_board != null)
+            {
+                _board.Paint(e.Graphics);
+            }
         }
     }
 }
